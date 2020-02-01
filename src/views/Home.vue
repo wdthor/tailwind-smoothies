b2fa578f2456489ac790ef60034b63165bfc822c
webdevthierry <thierry.hor.sd@gmail.com>
2020-02-01 18:30:51 +0100

    Feature : add template cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex justify-around flex-wrap mx-4">
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4">
+      <div class="card flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-xl mx-4">
+        <p class="text-blue-800 text-2xl font-semibold">HTML</p>
+        <p class="text-justify mt-4">Introduction à Html</p>
+      </div>
+    </div>
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4">
+      <div class="card flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-xl mx-4">
+        <p class="text-blue-800 text-2xl font-semibold">CSS</p>
+        <p class="text-justify mt-4">Styliser son site avec CSS</p>
+      </div>
+    </div>
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4">
+      <div class="card flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-xl mx-4">
+        <p class="text-blue-800 text-2xl font-semibold">JavaScript</p>
+        <p class="text-justify mt-4">Introduction à l'algorithmie</p>
+      </div>
+    </div>
+    <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4">
+      <div class="card flex flex-col items-center bg-gray-400 p-10 rounded-lg shadow-xl mx-4">
+        <p class="text-blue-800 text-2xl font-semibold">Adobe XD</p>
+        <p class="text-justify mt-4">J'apprend à faire mes maquettes</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
