db2b3534d6bf479f87b31f96a033e1f2d152cc48
webdevthierry <thierry.hor.sd@gmail.com>
2020-02-04 05:58:06 +0100

    Feature : add ingredients to smoothie recipe in form

--- src/views/AddSmoothie.vue
+++ src/views/AddSmoothie.vue
@@ -18,22 +18,59 @@
                         placeholder="Smoothie Name"
                         required
                         v-model="smoothieName"
-
                     />
                 </div>
                 <div class="mb-6">
+                    <div class="mb-4" v-for="(ingredient, index) in ingredients" :key="index">
+                        <label
+                            class="block text-blue-800 text-sm font-bold mb-2"
+                            for="ingredient"
+                        >Ingredient :</label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-800 leading-tight focus:outline-none focus:shadow-outline"
+                         type="text" name="ingredient" id="ingredient" v-model="ingredients[index]">
+                    </div>
                     <label
                         class="block text-blue-800 text-sm font-bold mb-2"
-                        for="ingredients"
-                    >Ingredients</label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-800 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="ingredients"
-                        type="text"
-                        placeholder="Ingredients"
-                        required
-                    />
-                    <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+                        for="add-ingredient"
+                    >Add an ingredient</label>
+                    <div class="flex flex-row items-center mb-2">
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-blue-800 leading-tight focus:outline-none focus:shadow-outline"
+                            id="add-ingredient"
+                            name="add-ingredient"
+                            type="text"
+                            placeholder="Ingredients"
+                            v-model="ingredient"
+                            @keydown.enter.prevent
+                            @keyup.enter="addIngredient"
+                        />
+                        <button
+                            class="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white p-1 ml-2 border border-blue-500 hover:border-transparent rounded"
+                            type="button"
+                            @keydown.enter.prevent
+                            @click.prevent="addIngredient"
+                            @keyup.enter="addIngredient"
+                        >
+                            <svg
+                                class="h-8 w-8 fill-current"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    class="heroicon-ui"
+                                    d="M17 11a1 1 0 010 2h-4v4a1 1 0 01-2 0v-4H7a1 1 0 010-2h4V7a1 1 0 012 0v4h4z"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <p
+                        class="text-blue-500 text-xs italic"
+                    >Press enter to add a new ingredient or click on the + button.</p>
+                    <p
+                        v-if="warningMessage"
+                        class="text-red-500 text-xs italic"
+                    >{{ warningMessage }}</p>
                 </div>
                 <div class="flex items-center justify-between">
                     <button
@@ -50,12 +87,31 @@ export default {
     name: "AddSmoothie",
     data() {
         return {
-            smoothieName: null
+            smoothieName: null,
+            ingredients: [],
+            ingredient: null,
+            warningMessage: null
         };
     },
     methods: {
         addSmoothie() {
             console.log(this.smoothieName);
+            console.log(this.ingredients);
+        },
+        addIngredient() {
+            /*  Check if ingredient input is not empty
+                    Then push the ingredient in ingredients array
+                    empty ingredient input and warning message
+                else
+                    display warning message
+            */
+            if (this.ingredient) {
+                this.ingredients.push(this.ingredient);
+                this.ingredient = null;
+                this.warningMessage = null;
+            } else {
+                this.warningMessage = "Enter a value to add an ingredient";
+            }
         }
     }
 };
